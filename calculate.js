5bb3d270f0adf6a0b64c342c215ed9ec3d0e54e4
Nguyên Nguyễn <khoi.nguyen.8602@gmail.com>
2022-10-12 11:45:58 -0700

    Updated lower and upper bounds

--- calculate.js
+++ calculate.js
@@ -15,10 +15,26 @@ function handleFileLoad(event) {
 //   document.getElementById("fileContent").textContent = event.target.result;
 }
 
-function findMinOf(letter) {
-    grade = document.getElementById("A");
+function findValueOf(letter) {
+    // let grade = document.querySelector("#letter")
+
+    // grade.addEventListener("input", function () {
+    //     let val = grade.nodeValue
+    //     console.log(val)
+    // })
+
+    let grade = document.getElementById(letter).value;
     console.log(grade)
-    
+    return grade;
 }
 
-findMinOf("A+");
+var input = document.querySelectorAll("input[type=number]")
+for (let i = 1; i < input.length; i++){
+    input[i].addEventListener("change", function () {
+        if (input[i-1].id != "max")
+            document.getElementById(input[i - 1].id).min = findValueOf(input[i].id);
+        if (i + 1 < length)
+            document.getElementById(input[i + 1].id).max = findValueOf(input[i].id);
+            
+    })
+}
