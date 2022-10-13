f2672f756a0f9fca74583f2630c4b390388ecac3
Nguyên Nguyễn <khoi.nguyen.8602@gmail.com>
2022-10-13 16:27:00 -0700

    Updated display of histogram

--- calculate.js
+++ calculate.js
@@ -8,12 +8,10 @@ function handleFileSelect(event) {
   const reader = new FileReader();
   reader.onload = handleFileLoad;
     reader.readAsText(event.target.files[0]);
-    console.log(reader);
 }
 
 function findValueOf(letter) {
     let grade = document.getElementById(letter).value;
-    console.log(grade)
     return grade;
 }
 
@@ -30,30 +28,16 @@ for (let i = 1; i < input.length; i++){
 
 
 function handleFileLoad(event) {
-//   document.getElementById("fileContent").textContent = event.target.result;
     data = event.target.result;
 
     grades = data.match(/[+-]?\d+(\.\d+)?/g);
-    
-    console.log(data)
-    console.log(grades)
-    console.log(input)
-
 
     let pair = data.split('\r\n')
-    // console.log(pair)
-
-    // num = pair[1].match(/[+-]?\d+(\.\d+)?/g);
-    // console.log(num)
-
-    // if (num == grades[0]) {
-    //     console.log("It works")
-    //     console.log(pair[1]) // pair of name and grade with "," 
-        console.log(pair[1].split(",")[0].trim()) // return the name and the grade
-    // }
+    
     var nums = grades.map((grade) => {
         return parseFloat(grade);
     });
+
     var max = Math.max(...nums);
     var maxIndex = nums.indexOf(max);
     document.getElementById("Highest").innerHTML = pair[maxIndex + 1].split(",")[0].trim() + " (" + max + "%)";
@@ -65,7 +49,6 @@ function handleFileLoad(event) {
     document.getElementById("Mean").innerHTML = mean(nums).toPrecision(4);
 
     document.getElementById("Median").innerHTML = median(nums);
-    // var histogram = document.querySelectorAll()
 
     for (let i = 0; i < grades.length; i++){
         for (let j = 1; j < input.length; j++){
@@ -73,26 +56,16 @@ function handleFileLoad(event) {
               parseFloat(grades[i]) >=
               parseFloat(document.getElementById(input[j].id).value)
             ) {
-                document.getElementById(j).innerHTML += "O";
+                document.getElementById(j).innerHTML += "👨‍🎓";
                 break;
             }
         }
-
-
-        // if (parseFloat(grades[i]) >= parseFloat(document.getElementById("A+").value)) {
-        //   document.getElementById("0").innerHTML += "O";
-        // } else if (parseFloat(grades[i]) >= parseFloat(document.getElementById("A").value)) {
-        //   document.getElementById("1").innerHTML += "O";
-        // } else if (parseFloat(grades[i]) >= parseFloat(document.getElementById("A-").value)) {
-        //     document.getElementById("2").innerHTML += "O";
-        // }
     }
 }
 
 function median(nums) {
     nums.sort(function (a, b) { return a - b; });
-    console.log(nums)
-    console.log(Math.floor(nums.length / 2));
+
     if (nums.length % 2 == 1) {
         return nums[Math.floor(nums.length / 2)]
     }
@@ -117,11 +90,8 @@ for (let k = 1; k < input.length; k++){
 
       for (let i = 0; i < grades.length; i++) {
         for (let j = 1; j < input.length; j++) {
-          if (
-            parseFloat(grades[i]) >=
-            parseFloat(document.getElementById(input[j].id).value)
-          ) {
-            document.getElementById(j).innerHTML += "O";
+          if (parseFloat(grades[i]) >= parseFloat(document.getElementById(input[j].id).value)) {
+            document.getElementById(j).innerHTML += "👨‍🎓";
             break;
           }
         }
